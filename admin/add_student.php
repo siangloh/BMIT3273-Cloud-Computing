3518d384be73c4b75d1244edafd25b6b38554fde
huiling11204 <huiling11204@gmail.com>
2025-09-13 23:32:17 +0800

    bucket updated

--- admin/add_student.php
+++ admin/add_student.php
@@ -23,7 +23,7 @@ $s3Client = new S3Client([
 ]);
 
 // Bucket name in S3
-$bucketName = 'student-web-bucket';  
+$bucketName = 'assm-student-web-bucketss';  
 
 if (is_post()) {
     // get data filled in the form
